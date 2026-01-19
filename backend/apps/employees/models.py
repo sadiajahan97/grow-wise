8bf60795af88761e327acad617b30ac9c09e5ab9
S-Delowar <sayed.buet97@gmail.com>
2026-01-19 13:32:10 +0600

    Backend updates

---
+++ backend/apps/employees/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from apps.organization.models import Department, Designation
+
+class Employee(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="employee"
+    )
+
+    staff_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    designation = models.ForeignKey(Designation, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.staff_id
