24776caac838944a4f0101804c3c3592a6a9d845
S-Delowar <sayed.buet97@gmail.com>
2026-01-20 09:21:48 +0600

    update backend - serve recommendation from db

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 
 from apps.authentication.api import LoginAPIView
 from apps.recommendations.api import RecommendationAPIView
+from apps.recommendations.api_2 import RecommendationFromDBAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/login/", LoginAPIView.as_view(), name="login"),
     path("api/recommendations/", RecommendationAPIView.as_view(), name="recommendations"),
-
+    path("api/recommendations/from-db/", RecommendationFromDBAPIView.as_view(), name="recommendations-from-db"),
 ]
